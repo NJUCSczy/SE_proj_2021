8956ac613ef0682c6040e02bd15951a1f5ec23fc
Vladimirical <1250214413@qq.com>
2022-03-28 21:19:41 +0800

    界面跳转demo

---
+++ demo/刘岩崧/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Home from './Home';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import { BrowserRouter as Router,Route} from 'react-router-dom';
+class App extends React.Component {
+	render(){
+		return(
+
+      <Router >
+      <div>
+       <Route path="/" component={Home} />
+       <Route path="/Page1" component={Page1} />                                                                                                                                                                                                                                                                                   
+       <Route path="/Page2" component={Page2} />                                                                                                                                                                                                                                                                                                                                                                                                                                 
+     </div>
+     
+     </Router>
+
+		)
+ 
+		}
+	}
+export default App;
