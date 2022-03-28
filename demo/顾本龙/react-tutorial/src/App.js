99ed21334c7600062469cb215657a08f881d0379
gu16 <2351886624@qq.com>
2022-03-28 19:14:50 +0800

    对react进行构建的初步的使用

---
+++ demo/顾本龙/react-tutorial/src/App.js
@@ -0,0 +1,29 @@
+/*import React, {Component} from 'react'
+import Table from './Table'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Table />
+      </div>
+    )
+  }
+}
+
+export default App*/
+import React, {Component} from 'react'
+import Table from './Table'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Table />
+        <h1>    </h1>
+      </div>
+    )
+  }
+}
+
+export default App
