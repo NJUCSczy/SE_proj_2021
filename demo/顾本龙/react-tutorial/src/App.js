262a0b3f43afc38f98beaba4d594ee9e22af1738
gu16 <59818002+gu16@users.noreply.github.com>
2022-04-01 20:57:47 +0800

    以委托为例给出的各功能中的提交，审核，查询的简单框架（尚未进行优化和部分功能的实现）

--- demo/顾本龙/react-tutorial/src/App.js
+++ demo/顾本龙/react-tutorial/src/App.js
@@ -1,26 +1,65 @@
-/*import React, {Component} from 'react'
-import Table from './Table'
 
+import React, {Component} from 'react'
+import Table from './Table'
+/*
 class App extends Component {
   render() {
     return (
       <div className="container">
+         <h1>Hello, React!</h1>
         <Table />
+        <h1>    </h1>
       </div>
     )
   }
-}
-
-export default App*/
-import React, {Component} from 'react'
-import Table from './Table'
-
+}*/
 class App extends Component {
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        software: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        software: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        software: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        software: 'Bartender',
+      },
+    ],
+  }
+
+  removeCharacter = (index) => {
+    const {characters} = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
   render() {
+    const { characters } = this.state;
+
     return (
       <div className="container">
-        <Table />
-        <h1>    </h1>
+        <h1>委托管理</h1>
+        <h2>委托信息填报情况</h2>
+        <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+        <h2>  </h2>
+        <h2>添加新委托</h2>
+        <h3>Name: </h3>
+        <h3>Software:  </h3>
+        <h3>Add(button)</h3>
+        <h2>  </h2>
+        <h2>已接受的委托(第二个表格来存放新接受的委托)</h2>
       </div>
     )
   }
--- demo/顾本龙/react-tutorial/src/Table.js
+++ demo/顾本龙/react-tutorial/src/Table.js
@@ -1,41 +1,45 @@
 import React, {Component} from 'react'
 
-class Table extends Component {
-  render() {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-            <th>gender</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Charlie  h  </td>
-            <td>Janitor    </td>
-            <td>M</td>
-          </tr>
-          <tr>
-            <td>Mac    </td>
-            <td>Bouncer    </td>
-            <td>M</td>
-          </tr>
-          <tr>
-            <td>Dee    </td>
-            <td>Aspiring actress    </td>
-            <td>FM</td>
-          </tr>
-          <tr>
-            <td>Dennis    </td>
-            <td>Bartender    </td>
-            <td>FM</td>
-          </tr>
-        </tbody>
-      </table>
-    )
-  }
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Software</th>
+        <th>Delete</th>
+        <th>Accept</th>
+      </tr>
+    </thead>
+  );
 }
 
-export default Table
+const TableBody = props => { 
+  const rows = props.characterData.map((row, index) => {
+      return (
+          <tr key={index}>
+              <td>{row.name}</td>
+              <td>{row.software}</td>
+              <td>
+                <button onClick={() => props.removeCharacter(index)}>Delete</button>
+              </td>
+              <td>
+                <button onClick={() => props.removeCharacter(index)}>Accept</button>
+              </td>
+          </tr>
+      );
+  });
+
+  return <tbody>{rows}</tbody>;
+}
+
+const Table = (props) => {
+  const { characterData, removeCharacter} = props;
+      return (
+          <table>
+              <TableHeader />
+              <TableBody characterData={characterData} removeCharacter={removeCharacter}/>
+          </table>
+      );
+}
+
+export default Table;
