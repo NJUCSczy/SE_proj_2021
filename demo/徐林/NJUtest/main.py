7828ce0012f039ab020cf764d07cea22e4515a04
Lionel1201 <1063150582@qq.com>
2022-04-08 21:51:35 +0800

    token有bug

--- demo/徐林/NJUtest/main.py
+++ demo/徐林/NJUtest/main.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 # filename: main.py
 import web
+from handle import Handle
 
 urls = (
-    '/wx', 'Handle',
+    '/wx', 'Handle', # /wx是要匹配的正则表达式，Handle表示接受请求的类的名称
 )
 
-class Handle(object):
-    def GET(self):
-        return "hello, this is handle view"
+
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
