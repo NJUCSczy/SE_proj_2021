731469bbd51cc7612dec59ea1bfca8f8ea11e497
NJUCSczy <3067887178@qq.com>
2022-05-06 19:35:32 +0800

    用antd修改了下测试中心网站界面

--- demo_1.0/src/Home.js
+++ demo_1.0/src/Home.js
@@ -1,20 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "./Home.css";
 import Administration from "./Adiministration/Administration"
 
+import { Layout, Menu, Dropdown, Breadcrumb,Space } from 'antd';
+import { DownOutlined, SmileOutlined } from '@ant-design/icons';
 
-class Home extends React.Component{
-    handle(){
-        const w=window.open("","App/Administration")
+const { Header, Content, Footer, Sider } = Layout;
+
+
+class Home extends React.Component {
+    handle() {
+        const w = window.open("", "App/Administration")
         //console.log("'w' constructed!\n")
-        ReactDOM.render(<Administration/>,w.document)
+        ReactDOM.render(<Administration />, w.document)
         //console.log("React!!")
     }
 
-    render(){
-        return(
+    render() {
+        var mobile = require('is-mobile');
+        const infoMenu = (
+            <Menu
+            mode="inline"
+                items={[
+                    {
+                        label: (
+                            <a >
+                                机构信息
+                            </a>
+                        ),
+                    },
+                    {
+                        label: (
+                            <a>
+                                资质信息
+                            </a>
+                        ),
+                    },
+                    {
+                        label: (
+                            <a>
+                                政策法规
+                            </a>
+                        ),
+                    },
+                    {
+                        label: (
+                            <a>
+                                业务信息
+                            </a>
+                        ),
+                    },
+                    {
+                        label: (
+                            <a>
+                                新闻资讯
+                            </a>
+                        ),
+                    },
+                    {
+                        label: (
+                            <a>
+                                友情链接
+                            </a>
+                        ),
+                    },
+
+                ]}
+            />
+        );
+        return (
+            /*
             <div>
                 <div id="container">
                     <div id="header">
@@ -60,6 +117,46 @@ class Home extends React.Component{
                     </div>
                 </div>
             </div>
+            */
+
+            <Layout>
+                <Header style={{ position: "fixed", zIndex: 1, paddingLeft:mobile()?"0px":"50px",paddingRight:mobile()?"0px":"50px",width:"100%",}}>
+                    <Menu
+                        style={{margin:0,paddingInlineStart:0}}
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['0']}
+                        items={new Array(4).fill(null).map((_, index) => ({
+                            key: String(index + 1),
+                            label: {
+                                0: "首页",
+                                1: (<Link to="/login">登录</Link>),
+                                2: "操作",
+                                3: (<Dropdown overlay={infoMenu} placement="bottomCenter">
+                                    <a onClick={e => e.preventDefault()}>
+                                        <Space>
+                                            信息
+                                            <DownOutlined />
+                                        </Space>
+                                    </a>
+                                </Dropdown>)
+                            }[index],
+                        }))}
+                    />
+                </Header>
+                <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+                        Content
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+
         )
     }
 }
