859e833facdd0e30244ac1a3c9e86e4b8bef904c
gu16 <2351886624@qq.com>
2022-05-14 15:42:09 +0800

    创建委托查看的界面1

--- demo_1.0/src/Adiministration/ViewApplication/ViewApplication.js
+++ demo_1.0/src/Adiministration/ViewApplication/ViewApplication.js
@@ -45,9 +45,35 @@ function ViewApplication(props){
 
   return(
       <Form
-      name="软件项目委托测试申请书"
-
-      >申请 </Form>
+      name="软件项目委托测试申请书">
+          <Checkbox.Group >
+          <Col span={30}>
+            <Checkbox value="软件确认测试" style={{ lineHeight: '32px' }}>
+              软件确认测试
+            </Checkbox>
+          </Col>
+          <Col span={30}>
+            <Checkbox value="成果/技术鉴定测试" style={{ lineHeight: '32px' }}>
+              成果/技术鉴定测试
+            </Checkbox>
+          </Col>
+          <Col span={30}>
+            <Checkbox value="专项资金验收测试" style={{ lineHeight: '32px' }}>
+              专项资金验收测试
+            </Checkbox>
+          </Col>
+          <Checkbox value="其他" style={{ lineHeight: '32px' }} >
+            其他
+          </Checkbox>
+          <Form.Item
+            style={{ paddingLeft: 20 }}
+            name="测试类型(其他)"
+            rules={[{ required: (formData['测试类型(其他)'] === true), message: '请填写其他测试类型名称！' }]}
+          >
+            <Input style={{ padding: 0 }} disabled={formData['测试类型(其他)'] != true} />
+          </Form.Item>
+        </Checkbox.Group> 
+      </Form>
   )
 }
 
