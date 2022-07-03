228f5b72ebd98e521430c7ec0956de208381883d
NJUCSczy <3067887178@qq.com>
2022-07-03 15:51:18 +0800

    完善了保密协议和签章的回显

--- demo_1.0/src/UserActions/actions/ViewFilesPart/ViewSignature/ViewSignature.js
+++ demo_1.0/src/UserActions/actions/ViewFilesPart/ViewSignature/ViewSignature.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { DatePicker, Divider, Form, Select, InputNumber, Switch, Radio, Slider, Button, Upload, Rate, Checkbox, Row, Col, Input } from 'antd';
 import './ViewSignature.css'
-import { getStageByInfo, getStatusInfo, USE_JSON_SERVER,REMOTE_SERVER } from '../../../functions/functions'
+import { getStageByInfo, getStatusInfo,getStageByDelegationState, USE_JSON_SERVER,REMOTE_SERVER } from '../../../functions/functions'
 import { useEffect, useState } from 'react';
 import moment from 'moment';
 
@@ -32,6 +32,7 @@ function ViewSignature(props) {
                             const newData = _.cloneDeep(prev)
                             newData['委托人签章'] = data['测试合同']['签章']['委托人签章']
                             newData['市场部签章'] = data['测试合同']['签章']['市场部签章']
+                            
                             return newData
                         })
                     }
@@ -63,8 +64,10 @@ function ViewSignature(props) {
                     if (data != null) {
                         setEntrustData(prev => {
                             const newData = _.cloneDeep(prev)
-                            newData['委托人签章'] = data['contractTable']['软件委托测试合同(委托方部分)']
-                            newData['市场部签章'] = data['contractTable']['软件委托测试合同(受托方部分)']
+                            newData['委托人签章'] = data['contractTable']['ContractTablePartyA']
+                            newData['市场部签章'] = data['contractTable']['ContractTablePartyB']
+                            newData['formData']=data
+                            console.log(newData)
                             return newData
                         })
                     }
@@ -85,7 +88,7 @@ function ViewSignature(props) {
 
                 <h2 style={{ fontWeight: 'bolder', fontSize: 30, marginTop: 40 }}>软件测试委托合同签章(委托方)</h2>
 
-                {getStageByInfo(entrustData['formData']) < 19 ? null : (
+                {(USE_JSON_SERVER? getStageByInfo(entrustData['formData']):(getStageByDelegationState(entrustData['formData']['contractState']))) < 19 ? null : (
                     <div>
                         <h3 style={{ fontWeight: 'bolder', marginTop: 30 }}>单位全称</h3>
                         <Input style={{ maxWidth: 500 }} disabled={true} defaultValue={entrustData['委托人签章']['单位全称']} />
@@ -120,7 +123,7 @@ function ViewSignature(props) {
                 )}
 
                 <h2 style={{ fontWeight: 'bolder', fontSize: 30, marginTop: 40 }}>软件测试委托合同签章(受托方)</h2>
-                {getStageByInfo(entrustData['formData']) < 18 ? null : (
+                {USE_JSON_SERVER? getStageByInfo(entrustData['formData']):getStageByDelegationState(entrustData['formData']['contractState']) < 18 ? null : (
                     <div>
                         <h3 style={{ fontWeight: 'bolder', marginTop: 30 }}>单位全称</h3>
                         <Input style={{ maxWidth: 500 }} disabled={true} defaultValue={'南京大学计算机软件新技术国家重点实验室'} />
